645d308952bf55247a51ebde571a13be3447fd28
Omid The Great <ooo342mmm@gmail.com>
2024-02-18 20:00:48 +0330

    change: general changes to compile.py

--- compile.py
+++ compile.py
@@ -1,46 +1,29 @@
 import subprocess
 import os
-
+import glob
 
 CURRENT_DIRECTORY = os.getcwd()
-XRAY_API_PATH = CURRENT_DIRECTORY + '/xray_api/'
-DIST_PATH = CURRENT_DIRECTORY + '/dist/'
+XRAY_API_PATH = os.path.join(CURRENT_DIRECTORY, 'xray_api')
+DIST_PATH = os.path.join(CURRENT_DIRECTORY, 'dist')
 
-PYTHON_OUTPUT_PATH = DIST_PATH + "python/"
-
-
-def find_proto_folders():
-    proto_folders = []
-    for root, dirs, files in os.walk(XRAY_API_PATH):
-        if any(file.endswith('.proto') for file in files):
-            proto_folders.append(root)
-    relative_proto_folders = []
-    for folder in proto_folders:
-        relative_proto_folders.append(os.path.relpath(folder, CURRENT_DIRECTORY))
-    return relative_proto_folders
+PYTHON_OUTPUT_PATH = os.path.join(DIST_PATH, "python")
 
 
-def convert_to_proto_path(paths):
-    proto_paths = []
-    for path in paths:
-        proto_paths.append(f"--proto_path={path}")
-    return proto_paths
+def find_proto_files():
+    return glob.glob(os.path.join(XRAY_API_PATH, '**', '*.proto'), recursive=True)
 
 
-# def convert_to_all_proto(paths):
-#     all_protos = []
-#     for path in paths:
-#         all_protos.append(f"{path}/*.proto")
-#     return all_protos
+proto_files = find_proto_files()
 
-
-proto_relative_folders = find_proto_folders()
-for_command = convert_to_proto_path(proto_relative_folders)
-# for_command2 = convert_to_all_proto(proto_relative_folders)
-
-command = ["protoc"] + for_command + [f"--python_out={PYTHON_OUTPUT_PATH}"] + ["$(find xray_api -name '*.proto')"]
+command = [
+    "protoc",
+    *[f"--proto_path={XRAY_API_PATH}"],
+    f"--python_out={PYTHON_OUTPUT_PATH}",
+    *proto_files
+]
 
 try:
     subprocess.check_output(command)
+    print("Proto files compiled successfully.")
 except subprocess.CalledProcessError as e:
     print(f"Error: {e}")

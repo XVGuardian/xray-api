8e5b574020b70b0df65ff04577557f057ef4bf52
Omid The Great <ooo342mmm@gmail.com>
2024-02-21 14:43:13 +0330

    add: cpp compiler added

--- compile.py
+++ compile.py
@@ -1,13 +1,14 @@
 import subprocess
 import os
 import glob
-
+import sys
 
 CURRENT_DIRECTORY = os.getcwd()
 XRAY_API_PATH = os.path.join(CURRENT_DIRECTORY, 'xray_api')
 DIST_PATH = os.path.join(CURRENT_DIRECTORY, 'dist')
 
 PYTHON_OUTPUT_PATH = os.path.join(DIST_PATH, "python")
+CPP_OUTPUT_PATH = os.path.join(DIST_PATH, "cpp")
 
 
 def find_proto_files():
@@ -16,13 +17,34 @@ def find_proto_files():
 
 proto_files = find_proto_files()
 
-command = (
-    f"python -m grpc_tools.protoc "
-    f"--proto_path={XRAY_API_PATH} "
-    f"--python_out={PYTHON_OUTPUT_PATH} "
-    f"--grpc_python_out={PYTHON_OUTPUT_PATH} "
-    f"{' '.join(proto_files)}"
-)
+
+match sys.argv[1]:
+    case "python":
+        command = (
+            f"python -m grpc_tools.protoc "
+            f"--proto_path={XRAY_API_PATH} "
+            f"--python_out={PYTHON_OUTPUT_PATH} "
+            f"--grpc_python_out={PYTHON_OUTPUT_PATH} "
+            f"{' '.join(proto_files)}"
+        )
+    case "cpp":
+        command = (
+            f"protoc "
+            f"--proto_path={XRAY_API_PATH} "
+            f"--cpp_out={CPP_OUTPUT_PATH} "
+            f"--grpc_out={CPP_OUTPUT_PATH} "
+            "--plugin=protoc-gen-grpc=$(which grpc_cpp_plugin) "
+            f"{' '.join(proto_files)}"
+        )
+    case _:
+        print("warning: not valid input! we will compile for python")
+        command = (
+            f"python -m grpc_tools.protoc "
+            f"--proto_path={XRAY_API_PATH} "
+            f"--python_out={PYTHON_OUTPUT_PATH} "
+            f"--grpc_python_out={PYTHON_OUTPUT_PATH} "
+            f"{' '.join(proto_files)}"
+        )
 
 exit_code = os.system(command)
 
